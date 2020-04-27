32d825dafc7c1f49aed7f30f71780fba267e6f9b
Utkarsh Agnihotri <56087687+utkhagni13@users.noreply.github.com>
2020-04-27 10:19:39 +0530

    Create Equalize the array.cpp

---
+++ hackerrank/problem solving/Equalize the array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cin >> n;
+    int arr[n] , freq , max = 0 , ans;
+
+    for(i=0;i<n;i++)
+    cin >> arr[i];
+
+    for(i=0;i<n;i++)
+    {
+        freq = 0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i] == arr[j])
+            freq++;
+        }
+
+        if(freq > max)
+        max = freq;
+    }
+
+    ans = n-max;
+    cout << ans;
+    return 0;
+}
